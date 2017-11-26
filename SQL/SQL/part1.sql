8b37cbec62a7f8b77b3acfa83509bd2dbb130782
claudia <claudiapopescu1796@gmail.com>
2017-11-26 13:56:04 +0200

    sql

--- SQL/SQL/part1.sql
+++ SQL/SQL/part1.sql
@@ -1,10 +1,11 @@
-
+--------------------------------------------------TABLES-----------------
 CREATE TABLE [Users](
 	[UserID] uniqueidentifier NOT NULL,
 	[UserName] nvarchar (50) NOT NULL,
 	[Password] nvarchar (50) NOT NULL,
 	[UserType] nvarchar (50) NOT NULL,  --ADMINISTRATOR or STANDARD
 CONSTRAINT [PK_UserID] PRIMARY KEY ([UserID]));
+GO
 
 CREATE TABLE [Articles](
 	[ArticleID] uniqueidentifier NOT NULL,
@@ -12,11 +13,13 @@ CREATE TABLE [Articles](
 	[Author] nvarchar (100),
 	[Description] nvarchar(max)
 CONSTRAINT [PK_News] PRIMARY KEY ([ArticleID]));
+GO
 
 CREATE TABLE [Photos](
 	[PhotoID] uniqueidentifier NOT NULL,
 	[PhotoVarbinary] varbinary(max),
 CONSTRAINT [PK_Photos] PRIMARY KEY ([PhotoID]));
+GO
 
 CREATE TABLE [ArticlesPhotos](
 	[ArticleID] uniqueidentifier NOT NULL,
@@ -26,6 +29,7 @@ CONSTRAINT [FK_ArticlesPhotos_Articles] FOREIGN KEY ([ArticleID])
     REFERENCES [Articles]([ArticleID]),
 CONSTRAINT [FK_ArticlesPhotos_Photos] FOREIGN KEY ([PhotoID])
     REFERENCES [Photos]([PhotoID]));
+GO
 
 CREATE TABLE [Players](
 	[PlayerID] uniqueidentifier NOT NULL,
@@ -34,6 +38,7 @@ CREATE TABLE [Players](
 	[BirthDay] date,
 CONSTRAINT [PK_PlayerID] PRIMARY KEY ([PlayerID]),	
 );
+GO
 
 CREATE TABLE [PlayersPhotos](
 	[PlayerID] uniqueidentifier NOT NULL,
@@ -44,13 +49,14 @@ CONSTRAINT [FK_PlayersPhotos_Players] FOREIGN KEY ([PlayerID])
 CONSTRAINT [FK_PlayersPhotos_Photos] FOREIGN KEY ([PhotoID])
     REFERENCES [Photos]([PhotoID])
 );
-
+GO
 
 CREATE TABLE [Positions](
 	[PositionID] uniqueidentifier NOT NULL,
 	[PositionName] nvarchar(50),
 CONSTRAINT [PK_PositionID] PRIMARY KEY ([PositionID]),
 );
+GO
 
 CREATE TABLE [PlayersPositions](
 	[PlayerID] uniqueidentifier NOT NULL,
@@ -61,12 +67,530 @@ CONSTRAINT [FK_PlayersPositions_Players] FOREIGN KEY ([PlayerID])
 CONSTRAINT [FK_PlayersPositions_Positions] FOREIGN KEY ([PositionID])
 	REFERENCES [Positions]([PositionID])
 );
+GO
 
 
-
-CREATE TABLE [Events](
+CREATE TABLE [NextEvents] (
 	[EventID] uniqueidentifier NOT NULL,
 	[EventName] nvarchar (50) NOT NULL,
 	[Location] nvarchar (50) NOT NULL,
 	[Date] date,
 CONSTRAINT [PK_EventID] PRIMARY KEY ([EventID]));
+GO
+
+------------------------------------------------CREATE-----------------------------------------------------------------
+CREATE PROCEDURE [dbo].[Articles_Create]
+(
+@ArticleID uniqueidentifier,
+@Title nvarchar(100),
+@Author nvarchar(100),
+@Description nvarchar(max)
+)
+	AS
+	 BEGIN
+		INSERT INTO Articles([ArticleID],[Title],[Author]) VALUES(@ArticleID, @Title, @Author)
+	END
+GO
+
+
+CREATE PROCEDURE [dbo].[NextEvents_Create]
+(
+@EventID uniqueidentifier,
+@EventName nvarchar(50),
+@Location nvarchar(50),
+@Date date
+)
+	AS
+	 BEGIN
+		INSERT INTO NextEvents([EventID],[EventName],[Location],[Date]) VALUES (@EventID,@EventName,@Location,@Date)
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Photos_Create]
+(
+@PhotoID uniqueidentifier,
+@PhotoVarbinary varbinary(max)
+)
+	AS
+	 BEGIN
+		INSERT INTO Photos([PhotoID],[PhotoVarbinary]) VALUES (@PhotoID,@PhotoVarbinary)
+	END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[ArticlesPhotos_Create]
+(
+@ArticleID uniqueidentifier,
+@PhotoID uniqueidentifier
+)
+	AS
+	 BEGIN
+		INSERT INTO ArticlesPhotos ([ArticleID],[PhotoID]) VALUES (@ArticleID,@PhotoID) 
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Players_Create]
+(
+@PlayerID uniqueidentifier,
+@PlayerName nvarchar(200),
+@Goals int,
+@BirthDay date
+)
+	AS
+	 BEGIN
+		INSERT INTO Players ([PlayerID],[PlayerName],[Goals],[Birthday]) VALUES (@PlayerID,@PlayerName,@Goals,@BirthDay)
+	 END
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[PlayersPhotos_Create]
+(
+@PlayerID uniqueidentifier,
+@PhotoID uniqueidentifier
+)	
+	AS
+	 BEGIN
+		INSERT INTO PlayersPhotos([PlayerID],[PhotoID]) VALUES (@PlayerID,@PhotoID)
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Positions_Create]
+(
+@PositionID uniqueidentifier,
+@PositionName nvarchar(50)
+)
+	AS
+	 BEGIN
+		INSERT INTO Positions ([PositionID],[PositionName]) VALUES (@PositionID,@PositionName)
+	 END
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[PlayersPositions_Create]
+(
+@PlayerID uniqueidentifier,
+@PositionID uniqueidentifier
+)
+	AS
+	 BEGIN
+		INSERT INTO PlayersPositions([PlayerID],[PositionID]) VALUES (@PlayerID,@PositionID) 
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Users_Create]
+(
+@UserID uniqueidentifier,
+@UserName nvarchar(50),
+@Password nvarchar(50),
+@UserType nvarchar(50)
+)
+	AS
+	 BEGIN
+		INSERT INTO Users([UserID],[UserName],[Password],[UserType]) VALUES (@UserID,@UserName,@Password,@UserType)
+	 END
+GO
+
+
+
+---------------------------------------------------UPDATE----------------------------------------------------------------------------
+
+CREATE PROCEDURE [dbo].[Articles_Update]
+(
+@ArticleID uniqueidentifier,
+@Title nvarchar(100),
+@Author nvarchar(100),
+@Description nvarchar(max)
+)
+	AS
+	 BEGIN
+		UPDATE Articles
+			SET Title=@Title,
+				Author=@Author
+			WHERE ArticleID=@ArticleID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[NextEvents_Update]
+(
+@EventID uniqueidentifier,
+@EventName nvarchar(50),
+@Location nvarchar(50),
+@Date date
+)
+	AS
+	 BEGIN
+		UPDATE NextEvents
+			SET EventName=@EventName,
+				Location=@Location,
+				Date=@Date
+			WHERE EventID=@EventID
+	 END
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[Photos_Update]
+(
+@PhotoID uniqueidentifier,
+@PhotoVarbinary varbinary(max)
+)
+	AS
+	 BEGIN
+		UPDATE Photos
+			SET PhotoVarbinary=@PhotoVarbinary
+			WHERE PhotoID=@PhotoID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Players_Update]
+(
+@PlayerID uniqueidentifier,
+@PlayerName nvarchar(200),
+@Goals int,
+@BirthDay date
+)
+	AS
+	 BEGIN
+		UPDATE Players
+			SET PlayerName=@PlayerName,
+			Goals=@Goals,
+			BirthDay=@BirthDay
+			WHERE PlayerID=@PlayerID
+	END
+GO
+
+
+CREATE PROCEDURE [dbo].[Positions_Update]
+(
+@PositionID uniqueidentifier,
+@PositionName nvarchar(50)
+)
+	AS
+	 BEGIN
+		UPDATE Positions
+			SET PositionName=@PositionName
+			WHERE PositionID=@PositionID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Users_Update]
+(
+@UserID uniqueidentifier,
+@UserName nvarchar(50),
+@Password nvarchar(50),
+@UserType nvarchar(50)
+)
+	AS
+	 BEGIN
+		UPDATE Users
+			SET UserName=@UserName,
+			Password=@Password,
+			UserType=@UserType
+			WHERE UserID=@UserID
+		END
+GO
+
+
+
+----------------------------------------------------ReadById-----------------------------------------------------------------------------
+
+CREATE PROCEDURE [dbo].[Users_ReadById]
+(
+@UserID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT u.UserID,
+			   u.Username,
+			   u.Password,
+			   u.UserType
+		FROM Users u
+		WHERE UserID=@UserID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[NextEvents_ReadById]
+(
+@EventID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT e.EventID,
+			   e.EventName,
+			   e.Location,
+			   e.Date
+		FROM NextEvents e
+		WHERE EventID=@EventID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Articles_ReadById]
+(
+@ArticleID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT a.ArticleID,
+			   a.Title,
+			   a.Author,
+			   a.Description
+		FROM Articles a
+		WHERE a.ArticleID=@ArticleID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Photos_ReadById]
+(
+@PhotoID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT p.PhotoID,
+			   p.PhotoVarbinary
+		FROM Photos p
+		WHERE p.PhotoID=@PhotoID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Players_ReadById]
+(
+@PlayerID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT p.PlayerID,
+			   p.PlayerName,
+			   p.Goals,
+			   p.BirthDay
+		FROM Players p
+		WHERE p.PlayerID=@PlayerID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Positions_ReadById]
+(
+@PositionID uniqueidentifier
+)
+	AS
+	 BEGIN
+		SELECT p.PositionID,
+			   p.PositionName
+		FROM Positions p
+		WHERE p.PositionID=@PositionID
+	 END
+GO
+
+
+------------------------------------------------------------DELETE-----------------------------------------------------------------------------
+
+
+CREATE PROCEDURE [dbo].[ArticlesPhotos_Delete]
+(
+@ArticleID uniqueidentifier,
+@PhotoID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM ArticlesPhotos 
+		WHERE ArticleID=@ArticleID AND PhotoID=@PhotoID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[PlayersPhotos_Delete]
+(
+@PlayerID uniqueidentifier,
+@PhotoID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM PlayersPhotos
+		WHERE PlayerID=@PlayerID AND PhotoID=@PhotoID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[PlayersPositions_Delete]
+(
+@PlayerID uniqueidentifier,
+@PositionID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM PlayersPositions 
+		WHERE PlayerID=@PlayerID AND PositionID=@PositionID
+	 END
+GO
+
+
+CREATE PROCEDURE [dbo].[Articles_Delete]
+(
+@ArticleID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM Articles
+		WHERE ArticleID=@ArticleID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[NextEvents_Delete]
+(
+@EventID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM NextEvents
+		WHERE EventID=@EventID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Photos_Delete]
+(
+@PhotoID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM Photos
+		WHERE PhotoID=@PhotoID
+	 END
+GO
+
+
+CREATE PROCEDURE [dbo].[Players_Delete]
+(
+@PlayerID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM Players
+		WHERE PlayerID=@PlayerID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Positions_Delete]
+(
+@PositionID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM Positions
+		WHERE PositionID=@PositionID
+	 END
+GO
+
+
+
+CREATE PROCEDURE [dbo].[Users_Delete]
+(
+@UserID uniqueidentifier
+)
+	AS
+	 BEGIN
+		DELETE FROM Users
+		WHERE UserID=@UserID
+	 END
+GO
+
+--------------------------------------------------------------ReadAll----------------------------
+CREATE PROCEDURE [dbo].[Articles_ReadAll]
+AS
+BEGIN
+	SELECT ArticleID,
+	       Title,
+	       Author,
+	       Description
+	FROM Articles
+END
+GO
+
+CREATE PROCEDURE [dbo].[Photos_ReadAll]
+AS
+BEGIN
+	SELECT PhotoID,
+	       PhotoVarbinary
+	FROM Photos
+END
+GO
+
+CREATE PROCEDURE [dbo].[Players_ReadAll]
+AS
+BEGIN
+	SELECT PlayerID,
+		   PlayerName,
+		   Goals,
+		   BirthDay
+	FROM Players
+END
+GO
+
+CREATE PROCEDURE [dbo].[Positions_ReadAll]
+AS
+BEGIN
+	SELECT PositionID,
+		   PositionName
+	FROM Positions
+END
+GO
+
+CREATE PROCEDURE [dbo].[NextEvents_ReadAll]
+AS
+BEGIN
+	SELECT EventID,
+		   EventName,
+		   Location,
+		   Date
+	FROM NextEvents
+END
+GO
+
+CREATE PROCEDURE [dbo].[Users_ReadAll]
+AS
+BEGIN
+	SELECT UserID,
+		   UserName,
+		   Password,
+		   UserType
+	FROM Users
+END
+GO
+
+
--- SQL/SQL/part2.sql
+++ SQL/SQL/part2.sql
@@ -261,127 +261,20 @@ INSERT INTO Positions([PositionID],[PositionName]) VALUES (@PositionID,'Centre')
 
 
 
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - CSM Oradea', 'Sala Sporturilor Horia Demian','2017-11-11');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - CSM Bucuresti', 'Sala Sporturilor Horia Demian','2017-11-19');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Viborg FDK- Blue Ravens', 'Blue Arena','2017-11-20');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'SCM Craiova- Blue Ravens', 'Sala Polivalenta Craiova','2017-11-25');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Buducnost Podgorica', 'Sala Sporturilor Horia Demian','2017-11-28');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Corona Brasov - Blue Ravens', 'Sala Sporturilor Brasov','2018-01-20');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Gyor Audi ETO', 'Sala Sporturilor Horia Demian','2018-02-15');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'CSM Slatina - Blue Ravens', 'Sala Sporturilor Slatina','2018-03-13');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Krim Lublijana', 'Sala Sporturilor Horia Demian','2018-03-20');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - HC Zalau', 'Sala Sporturilor Horia Demian','2018-03-29');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Ferencvaros', 'Sala Sporturilor Horia Demian','2018-04-01');
-INSERT INTO Events ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Vardar Skopje- Blue Ravens', 'Dobreva Arena','2018-04-13');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - CSM Oradea', 'Sala Sporturilor Horia Demian','2017-11-11');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - CSM Bucuresti', 'Sala Sporturilor Horia Demian','2017-11-19');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Viborg FDK- Blue Ravens', 'Blue Arena','2017-11-20');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'SCM Craiova- Blue Ravens', 'Sala Polivalenta Craiova','2017-11-25');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Buducnost Podgorica', 'Sala Sporturilor Horia Demian','2017-11-28');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Corona Brasov - Blue Ravens', 'Sala Sporturilor Brasov','2018-01-20');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Gyor Audi ETO', 'Sala Sporturilor Horia Demian','2018-02-15');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'CSM Slatina - Blue Ravens', 'Sala Sporturilor Slatina','2018-03-13');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Krim Lublijana', 'Sala Sporturilor Horia Demian','2018-03-20');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - HC Zalau', 'Sala Sporturilor Horia Demian','2018-03-29');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Blue Ravens - Ferencvaros', 'Sala Sporturilor Horia Demian','2018-04-01');
+INSERT INTO NextEvents ([EventID], [EventName], [Location], [Date]) VALUES (NEWID(),'Vardar Skopje- Blue Ravens', 'Dobreva Arena','2018-04-13');
 
 
 
---the number of articles with authors
-SELECT COUNT(Author) AS NoOfSignedArticles
-FROM Articles 
 
---players ordered desc by number of scored goals
-SELECT PlayerName, Goals 
-FROM Players
-ORDER BY Goals DESC
-
---players which have the letter 'u' in their names
-SELECT PlayerName
-FROM Players
-WHERE PlayerName LIKE '%u%'
-
---players that scored more than 10 goals 
-SELECT PlayerName, Goals
-FROM Players
-WHERE Goals>10
-
---select photos which are used in an article at least
-SELECT DISTINCT p.PhotoID
-FROM Photos p
-	INNER JOIN ArticlesPhotos a ON p.PhotoID=a.PhotoID
-
---select articles that have at least one photo
-SELECT DISTINCT a.ArticleID
-FROM Articles a
-	INNER JOIN ArticlesPhotos p ON p.ArticleID=a.ArticleID
-
---number of photos for each article with photos
-SELECT a.ArticleID, COUNT(a.ArticleID) AS NoOfPhotos
-FROM Articles a
-	INNER JOIN ArticlesPhotos p ON a.ArticleID= p.ArticleID
-GROUP BY a.ArticleID
-
---photos that have been used in more than 2 articles
-SELECT p.PhotoID, COUNT(p.PhotoID) AS NoOfArticles
-FROM Photos p
-	INNER JOIN ArticlesPhotos a ON a.PhotoID= p.PhotoID
-GROUP BY p.PhotoID
-HAVING COUNT(p.PhotoID) >2
-
---select all the photos, even the ones without one or more articles
-SELECT DISTINCT p.PhotoID
-FROM Photos p
-	LEFT JOIN ArticlesPhotos a ON p.PhotoID=a.PhotoID
-
---select only the photos who are used in at least one article
-SELECT DISTINCT p.PhotoID
-FROM Photos p
-	RIGHT JOIN ArticlesPhotos a ON p.PhotoID=a.PhotoID
-
---select all articles (the ones with photos as well as the ones without photos)
-SELECT DISTINCT a.ArticleID,a.Title
-FROM Articles a
-	FULL  OUTER JOIN ArticlesPhotos p ON a.ArticleID=p.ArticleID
-
---first 3 events from 2018
-SELECT TOP 3 EventName
-FROM Events E 
-WHERE E.Date>'2018-01-01'
-GO
-
---number of each type of users
-CREATE VIEW NoOfUsersForEachType
-AS
-		SELECT  u.UserType,COUNT(u.UserType) AS NoType
-		FROM Users u
-		GROUP BY u.UserType
-		HAVING COUNT(u.UserType) >1
-GO
-
-
-
-SELECT * from NoOfUsersForEachType
-GO
-
---returns the number of future events announced
-CREATE FUNCTION [GetEventsCount] ()
-RETURNS int
-AS
-BEGIN
-	RETURN(
-		SELECT COUNT(EventName)
-		FROM Events)
-END
-GO
-
-
-SELECT [dbo].[GetEventsCount] () AS NoOfFutureEvents
-GO
-
---this procedure selects only the articles that have a number of photos greater than a given value
-
-CREATE PROCEDURE [GetArticlesCountInArticlesPhotos]
-(
-	@Count nvarchar(MAX)
-)
-AS
-BEGIN
-	SELECT a.Title, COUNT(a.ArticleID) AS ArticlesWithNPhotos
-	FROM Articles a
-		LEFT JOIN ArticlesPhotos p ON a.ArticleID= p.ArticleID
-	GROUP BY a.Title
-	HAVING COUNT(a.ArticleID) > @Count
-END
-GO
-EXEC [GetArticlesCountInArticlesPhotos] @Count = 1
-GO
+	
